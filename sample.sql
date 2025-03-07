dca519e942c99a09d6e58e8b3155be0b67b9c9fb
Agnieszka <zlotaagnieszka81@o2.pl>
2025-03-07 13:02:49 +0100

    zrealizowane

--- sample.sql
+++ sample.sql
@@ -6,7 +6,7 @@ SELECT
     o.total_amount 
 FROM users u
 JOIN orders o ON u.id = o.user_id;
-/* drugi sql
+/* drugi sql Pobiera tylko zamówienia oczekujące
 SELECT 
     u.id AS user_id, 
     u.name AS user_name, 
@@ -16,4 +16,18 @@ SELECT
     o.status 
 FROM users u
 JOIN orders o ON u.id = o.user_id
-WHERE o.status = 'pending';  -- Pobiera tylko zamówienia oczekujące
+WHERE o.status = 'pending';  
+
+/* trzeci sql Pobiera tylko zamówienia zrealizowane
+SELECT 
+    u.id AS user_id, 
+    u.name AS user_name, 
+    o.id AS order_id, 
+    o.total_amount, 
+    p.payment_method, 
+    p.payment_status, 
+    p.payment_date
+FROM users u
+JOIN orders o ON u.id = o.user_id
+JOIN payments p ON o.id = p.order_id
+WHERE o.status = 'completed';
